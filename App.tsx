de13c6d0099b8840a6263321fc4f318eef5f1f41
dign1120 <dign1120@naver.com>
2025-04-12 02:02:06 +0900

    add 프리텐다드 폰트 설정 완료

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import {View, Text} from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-white text-lg">Hello, NativeWind!</Text>
+      <Text className="text-white text-lg font-extrabold">
+        Hello, NativeWind!
+      </Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        thin: ['Pretendard-Thin'],
+        extralight: ['Pretendard-ExtraLight'],
+        light: ['Pretendard-Light'],
+        regular: ['Pretendard-Regular'],
+        medium: ['Pretendard-Medium'],
+        semibold: ['Pretendard-SemiBold'],
+        bold: ['Pretendard-Bold'],
+        extrabold: ['Pretendard-ExtraBold'],
+        black: ['Pretendard-Black'],
+        sans: ['Pretendard-Regular'],
+      },
+    },
   },
   plugins: [],
 };
