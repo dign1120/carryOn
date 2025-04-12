524feba28adfda31af0ceb063fb004e21e6f137c
dign1120 <dign1120@naver.com>
2025-04-13 03:34:49 +0900

    add 스플래시 스크린 기능 추가됨 - android

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
       <Text className="text-white text-lg font-extrabold">
